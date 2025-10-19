83b951673e0830ced5d5dcf2fdc7ff42465e762f
liuxiaochao-satuo <2799851651@qq.com>
2025-10-19 22:57:45 +0800

    update ch04 two layer net

---
+++ ch04_me/two_layer_net_me.py
@@ -0,0 +1,5 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from common_me.functions_me import * 
+from common_me.gradient_me import numerical_gradient
+
