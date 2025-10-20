96ab09b141666955c7b2ae9de2751754f947bb34
liuxiaochao-satuo <2799851651@qq.com>
2025-10-20 20:06:02 +0800

    update ch04  net

--- ch04_me/train_neuralnet_me.py
+++ ch04_me/train_neuralnet_me.py
@@ -1,17 +1,16 @@
-# coding: utf-8
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # 为了导入父目录的文件而进行的设定
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  #
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
-from two_layer_net import TwoLayerNet
+from two_layer_net_me import TwoLayerNetME
 
 # 读入数据
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True,one_hot_label=True)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+network = TwoLayerNetME(input_size=784, hidden_size=50, output_size=10)
 
-iters_num = 10000  # 适当设定循环的次数
+iters_num = 10000 # 适当的循环次数
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
@@ -25,33 +24,33 @@ iter_per_epoch = max(train_size / batch_size, 1)
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
-    t_batch = t_train[batch_mask]
-    
+    t_bathch = t_train[batch_mask]
+
     # 计算梯度
-    #grad = network.numerical_gradient(x_batch, t_batch)
-    grad = network.gradient(x_batch, t_batch)
-    
+    #grad = network.numerical_gradient(x_batch, t_bathch)
+    grad = network.gradient(x_batch, t_bathch)
+
     # 更新参数
     for key in ('W1', 'b1', 'W2', 'b2'):
         network.params[key] -= learning_rate * grad[key]
-    
-    loss = network.loss(x_batch, t_batch)
+
+    loss = network.loss(x_batch, t_bathch)
     train_loss_list.append(loss)
-    
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
-        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+        print(f'epoch {i // iter_per_epoch} : train acc, test acc | {train_acc}, {test_acc}')
 
 # 绘制图形
-markers = {'train': 'o', 'test': 's'}
+markers = {'train':'o', 'test':'s'} 
 x = np.arange(len(train_acc_list))
 plt.plot(x, train_acc_list, label='train acc')
-plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.plot(x,test_acc_list, label='test acc', linestyle='--')
 plt.xlabel("epochs")
 plt.ylabel("accuracy")
 plt.ylim(0, 1.0)
-plt.legend(loc='lower right')
+plt.legend()
 plt.show()
