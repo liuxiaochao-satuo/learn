a57e0f0e3a9dc68d0cd6f2685b2c2066abbb9842
liuxiaochao-satuo <2799851651@qq.com>
2025-11-06 22:02:46 +0800

    update_ch06_11_6

--- ch06_me/optimizer_compare_native_me.py
+++ ch06_me/optimizer_compare_native_me.py
@@ -1,15 +1,12 @@
-# coding: utf-8
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # 为了导入父目录的文件而进行的设定
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
-from common.optimizer import *
-
+from common_me.optimizer_me import *
 
 def f(x, y):
     return x**2 / 20.0 + y**2
-
 
 def df(x, y):
     return x / 10.0, 2.0*y
@@ -20,12 +17,11 @@ params['x'], params['y'] = init_pos[0], init_pos[1]
 grads = {}
 grads['x'], grads['y'] = 0, 0
 
-
 optimizers = OrderedDict()
-optimizers["SGD"] = SGD(lr=0.95)
-optimizers["Momentum"] = Momentum(lr=0.1)
-optimizers["AdaGrad"] = AdaGrad(lr=1.5)
-optimizers["Adam"] = Adam(lr=0.3)
+optimizers['SGD'] = SGD(lr=0.95)
+optimizers['Momentum'] = Momentum(lr=0.1)
+optimizers['AdaGrad'] = AdaGrad(lr=1.5)
+optimizers['Adam'] = Adam(lr=0.3)
 
 idx = 1
 
@@ -34,37 +30,33 @@ for key in optimizers:
     x_history = []
     y_history = []
     params['x'], params['y'] = init_pos[0], init_pos[1]
-    
+
     for i in range(30):
         x_history.append(params['x'])
         y_history.append(params['y'])
         
         grads['x'], grads['y'] = df(params['x'], params['y'])
         optimizer.update(params, grads)
-    
 
     x = np.arange(-10, 10, 0.01)
     y = np.arange(-5, 5, 0.01)
-    
-    X, Y = np.meshgrid(x, y) 
+
+    X, Y = np.meshgrid(x, y)
     Z = f(X, Y)
-    
-    # for simple contour line  
+
+    # for simple contour line
     mask = Z > 7
     Z[mask] = 0
-    
-    # plot 
+
+    # plot
     plt.subplot(2, 2, idx)
     idx += 1
-    plt.plot(x_history, y_history, 'o-', color="red")
+    plt.plot(x_history, y_history, 'o-', color='red')
     plt.contour(X, Y, Z)
     plt.ylim(-10, 10)
     plt.xlim(-10, 10)
-    plt.plot(0, 0, '+')
-    #colorbar()
-    #spring()
     plt.title(key)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    
+    plt.xlabel('x')
+    plt.ylabel('y')
+
 plt.show()
