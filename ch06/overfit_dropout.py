830075de5914e23629e7ca2f01408bfe8399e17e
liuxiaochao-satuo <2799851651@qq.com>
2025-11-26 21:36:13 +0800

    update ch06 11-26

--- ch06/overfit_dropout.py
+++ ch06/overfit_dropout.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import os
 import sys
-sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # 为了导入父目录的文件而进行的设定
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
